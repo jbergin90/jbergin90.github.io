1c55db1b4bb390d3e8f62a8d979e0f58cda4d334
James Bergin <james@artiphon.com>
2016-10-17 21:54:32 -0500

    Added configs for different size screens

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 ---
 layout: null
 ---
+function doResizing(){
+  if($(window).matchMedia( "all and (max-width: 960px)" )) {
+      $('.panel-main').css('width', '100%')
+  } else {
+      $('.panel-main').css('width', '50%')
+  }
+}
+
 $(document).ready(function () {
+
   $('a.blog-button').click(function (e) {
+    function buttonAction(){
+      if (currentWidth < 960) {
+        $('.panel-cover').addClass('panel-cover--collapsed')
+        $('.content-wrapper').addClass('animated slideInRight')
+      } else {
+        $('.panel-cover').addClass('panel-cover--collapsed')
+        $('.panel-cover').css('max-width', currentWidth)
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+        $('.content-wrapper').addClass('animated slideInRight')
+      }
+    }
+
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
-    }
+    $('.panel-main').animate({'width': '100%'}, 200, swing = 'swing', function () {})
+    setTimeout(buttonAction, 200);
   })
 
   if (window.location.hash && window.location.hash == '#about') {
@@ -32,4 +48,9 @@ $(document).ready(function () {
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
   })
 
+  if ($('.panel-cover').hasClass('panel-cover--collapsed')){
+    $('.panel-main').css('width', '100%')
+  } else{
+    doResizing()
+  }
 })
