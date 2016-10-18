a946c824f89fd759601eee2136275259a74cfc6f
James Bergin <james@artiphon.com>
2016-10-17 22:50:10 -0500

    moar changes

--- js/main.js
+++ js/main.js
@@ -29,8 +29,7 @@ $(document).ready(function () {
       } else {
         $('.panel-cover').addClass('panel-cover--collapsed')
         $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').css('max-width', 200)
-        // $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+        $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
         $('.content-wrapper').addClass('animated slideInRight')
       }
     }
